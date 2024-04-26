079d980cd05c2e80480c1425eebfd0f5ad06f0ba
farzanapomy <farzanapomy56@gmail.com>
2024-04-27 02:49:19 +0600

    nav-change

---
+++ app/src/pages/Components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <div>
+      <h1 className=" mt-10 mb-6 text-2xl font-Inter font-bold  invert drop-shadow-xl">
+        TrɅ<span className="text-black">vel</span>lɅ Go
+      </h1>
+    </div>
+  );
+};
+
+export default Logo;
--- app/src/pages/Components/Navbar/Navbar.jsx
+++ app/src/pages/Components/Navbar/Navbar.jsx
@@ -1,11 +1,19 @@
+import Logo from "../Logo/Logo";
+
 const Navbar = () => {
   return (
-    <div className="w-full max-w-[1024px]   mx-auto">
-      <div>
-        <h1 className=" ">Travella App</h1>
-        <div>
-          <button>Register</button>
-          <button>Login</button>
+    <div className="w-full max-w-[1024px] border-b border-[#E7E7F2] border-opacity-40  mx-auto">
+      <div className="">
+        <div className="flex justify-between items-center">
+          <div className="mx-4 mt-10 mb-6">
+            <span>About</span>
+            <span>Contact</span>
+          </div>
+          <Logo/>
+          <div className="mx-4 mt-10 mb-6">
+            <button>Register</button>
+            <button>Login</button>
+          </div>
         </div>
       </div>
     </div>
