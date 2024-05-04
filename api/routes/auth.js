391f68139c644ff4428943a8206e5a8fb81079c2
farzanapomy <farzanapomy56@gmail.com>
2024-05-04 20:56:43 +0600

    change-dir

--- api/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { login, register } from '../controller/auth.js';
-
-const router = express.Router();
-
-router.post('/register-user', register);
-router.post('/login-user', login);
-
-export default router;
