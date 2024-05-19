0fd019c25974da9dddc88cc86c604bff8e81bd7c
farzanapomy <farzanapomy56@gmail.com>
2024-05-20 00:07:38 +0600

    nav-set

---
+++ app/src/pages/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div className="w-full max-w-[1024px] bg-black mx-auto">
+      <div>
+        <h1 className="text-pink-600">Travella App</h1>
+        <div>
+          <button>Register</button>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
