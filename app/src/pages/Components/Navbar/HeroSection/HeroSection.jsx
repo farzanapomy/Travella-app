1c9d90e478098d797ea8e347bb491289da545de7
farzanapomy <farzanapomy56@gmail.com>
2024-05-27 00:33:18 +0600

    updated

--- app/src/pages/Components/Navbar/HeroSection/HeroSection.jsx
+++ app/src/pages/Components/Navbar/HeroSection/HeroSection.jsx
@@ -2,8 +2,8 @@ import hero from '../../../../assets/hero.svg';
 const HeroSection = () => {
   return (
     <div className="  px-5">
-      <div className=" md:flex justify-between items-center  h-fit mt-22 relative">
-        <div className="w-4/6 mx-auto tracking-tight text-center">
+      <div className=" md:flex justify-between items-center  h-fit mt-18 relative">
+        <div className="w-4/6 mx-auto tracking-tight text-center md:text-start">
           <h1 className="text-5xl md:text-7xl font-bold font-Inter text-white">
             Make your travel wishlist, we’ll do the rest
           </h1>
