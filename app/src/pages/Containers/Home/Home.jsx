4fddb1f8adb412ff9a6c5219da4c769ea3693213
farzanapomy <farzanapomy56@gmail.com>
2024-05-27 16:43:03 +0600

    up

--- app/src/pages/Containers/Home/Home.jsx
+++ app/src/pages/Containers/Home/Home.jsx
@@ -6,8 +6,11 @@ import Hotels from '../Hotels/Hotels';
 const Home = () => {
   return (
     <div>
-      <div className="bg-[url('./assets/bg-hero.jpg')] h-[650px] object-fill  bg-cover bg-center bg-no-repeat opacity-90 bg-blend-multiply	object-left-bottom	 ">
-        <div className='max-w-[1224px] mx-auto'>
+      <div
+        className="bg-[url('./assets/bg-hero.jpg')] h-[650px] object-fill   bg-auto bg-center bg-no-repeat opacity-90 bg-blend-multiply	 "
+        style={{ objectFit: 'cover' }}
+      >
+        <div className="max-w-[1224px] mx-auto">
           <Navbar />
           <HeroSection />
         </div>
