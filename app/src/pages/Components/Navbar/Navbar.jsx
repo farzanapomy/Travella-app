bdc4b31a85631c6b33f02b46afd9dc73fc1b0bcd
farzanapomy <farzanapomy56@gmail.com>
2024-06-01 13:30:56 +0600

    updated-nav

--- app/src/pages/Components/Navbar/Navbar.jsx
+++ app/src/pages/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import Logo from '../Logo/Logo';
 
 const Navbar = () => {
   return (
-    <div className="w-full max-w-[1024px] border-b border-[#E7E7F2] border-opacity-40  mx-auto">
+    <div className="w-full border-b border-[#E7E7F2] border-opacity-40  mx-auto">
       <div className="">
         <div className="flex justify-between items-center">
           <div className="mx-4 mt-10 mb-6">
--- app/src/pages/Containers/Home/Home.jsx
+++ app/src/pages/Containers/Home/Home.jsx
@@ -1,11 +1,20 @@
+import LayOut from '../../Components/LayOut/LayOut';
+import HeroSection from '../../Components/Navbar/HeroSection/HeroSection';
 import Navbar from '../../Components/Navbar/Navbar';
+import Hotels from '../Hotels/Hotels';
 
 const Home = () => {
   return (
-    <div className="w-full h-full">
-      <div className="bg-[url('./assets/hero-bg.jpg')] h-[650px] object-contain  bg-cover bg-center bg-no-repeat opacity-90">
-        <Navbar />
+    <div>
+      <div className="bg-[url('./assets/bg-hero.jpg')] h-[650px] object-fill  bg-cover bg-center bg-no-repeat opacity-90 bg-blend-multiply	object-left-bottom	 ">
+        <div className='max-w-[1224px] mx-auto'>
+          <Navbar />
+          <HeroSection />
+        </div>
       </div>
+      <LayOut>
+        <Hotels />
+      </LayOut>
     </div>
   );
 };
