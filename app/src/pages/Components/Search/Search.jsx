a81971df717ed0555b9c072dff2e0c843a7b00d1
farzanapomy <farzanapomy56@gmail.com>
2024-06-12 02:25:38 +0600

    added-custom

--- app/src/pages/Components/Search/Search.jsx
+++ app/src/pages/Components/Search/Search.jsx
@@ -6,7 +6,7 @@ import 'react-date-range/dist/theme/default.css';
 import { FaBed } from 'react-icons/fa';
 import { IoMdPerson } from 'react-icons/io';
 import { SlCalender } from 'react-icons/sl';
-
+import './search.css';
 const Search = () => {
   const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
@@ -31,8 +31,8 @@ const Search = () => {
     });
   };
   return (
-    <div className=" ">
-      <div className=" w-5/6 md:w-5/6 h-fit mx-auto 	  rounded-md    absolute right-0 left-0 -bottom-80 md:-bottom-65 py-8 px-4 md:px-14  bg-[#E5E4CC]">
+    <div className="">
+      <div className=" w-5/6 md:w-5/6 h-fit mx-auto 	  rounded-md    absolute right-0 left-0 -bottom-80 md:-bottom-65 py-8 px-4 md:px-14  bg-[#e5e4cccc] ">
         <h1 className="mb-3 font-semibold text-2xl">Book your vacation </h1>
         <div className="flex flex-wrap lg:flex-nowrap w-fit md:w-full mx-auto justify-between items-center gap-3 lg:gap-0">
           <div className="flex justify-between items-center border border-[#979797] rounded-l-lg p-2 gap-2 w-full">
@@ -52,21 +52,23 @@ const Search = () => {
               <SlCalender />
             </div>
             <div className="relative ">
-              <span className="text-[#a19d9d] text-sm">
+              <span className="text-[#4e4a4a] text-sm">
                 <span onClick={() => setOpenDate(!openDate)}>
                   {`${moment(date[0]?.startDate).format('MM/DD/YYYY')}`} to{' '}
                   {`${moment(date[0]?.endDate).format('MM/DD/YYYY')}`}
                 </span>
               </span>
-              {openDate && (
-                <DateRange
-                  editableDateInputs={true}
-                  onChange={(item) => setDate([item.selection])}
-                  moveRangeOnFirstSelection={false}
-                  ranges={date}
-                  className="absolute top-8  right-0 "
-                />
-              )}
+              <div className=" bg-[#e5e4cccc]">
+                {openDate && (
+                  <DateRange
+                    editableDateInputs={true}
+                    onChange={(item) => setDate([item.selection])}
+                    moveRangeOnFirstSelection={false}
+                    ranges={date}
+                    className="absolute bg-[#e5e4cc]  top-8  right-0 "
+                  />
+                )}
+              </div>
             </div>
           </div>
           <div className="relative w-full flex  justify-between items-center border border-[#979797] p-2  gap-2 rounded-r-lg ">
@@ -75,7 +77,7 @@ const Search = () => {
             </div>
             <div className="">
               <span
-                className="text-[#a19d9d] text-sm "
+                className="text-[#4e4a4a] text-sm "
                 onClick={() => setOpenGuest(!openGuest)}
               >
                 {option?.adult} adult {option?.children} Children {option?.room}
@@ -83,18 +85,24 @@ const Search = () => {
               </span>
             </div>
             {openGuest && (
-              <div className="absolute top-11 right-0  bg-[#494545] p-6  text-sm z-10">
+              <div className="absolute top-10 right-0    shadow-3 bg-[#e5e4cccc] p-6  text-sm z-10 rounded-md">
                 <div className="flex justify-between items-center gap-4 mb-2">
                   <span>Adult</span>
                   <div className="flex justify-between items-center gap-2">
                     <button
                       onClick={() => handleButton('adult', 'd')}
                       disabled={option.adult <= 1}
+                      className={`${
+                        option.adult <= 0 && 'cursor-not-allowed'
+                      } border px-1.5`}
                     >
                       -
                     </button>
                     <span>{option.adult}</span>
-                    <button onClick={() => handleButton('adult', 'i')}>
+                    <button
+                      onClick={() => handleButton('adult', 'i')}
+                      className="border px-1.5"
+                    >
                       +
                     </button>
                   </div>
@@ -103,13 +111,17 @@ const Search = () => {
                   <span>Children</span>
                   <div className="flex justify-between items-center gap-2">
                     <button
+                      className="border px-1.5"
                       onClick={() => handleButton('children', 'd')}
                       disabled={option.children <= 0}
                     >
                       -
                     </button>
                     <span>{option.children}</span>
-                    <button onClick={() => handleButton('children', 'i')}>
+                    <button
+                      className="border px-1.5"
+                      onClick={() => handleButton('children', 'i')}
+                    >
                       +
                     </button>
                   </div>
@@ -120,12 +132,19 @@ const Search = () => {
                     <button
                       onClick={() => handleButton('room', 'd')}
                       disabled={option.room <= 1}
-                      className={`${option.room <= 1 && 'cursor-not-allowed'}`}
+                      className={`${
+                        option.room <= 1 && 'cursor-not-allowed'
+                      } border px-1.5`}
                     >
                       -
                     </button>
                     <span>{option.room}</span>
-                    <button onClick={() => handleButton('room', 'i')}>+</button>
+                    <button
+                      className="border px-1.5"
+                      onClick={() => handleButton('room', 'i')}
+                    >
+                      +
+                    </button>
                   </div>
                 </div>
               </div>
