eb405380edd30d749cb0f48cb05270a262a34a4b
farzanapomy <farzanapomy56@gmail.com>
2024-06-20 02:37:01 +0600

    up-sub

---
+++ app/src/pages/Components/Subscribe/Subscribe.jsx
@@ -0,0 +1,16 @@
+const Subscribe = () => {
+  return (
+    <div className="bg-[#3A5357] w-full mx-auto flex justify-center items-center p-10 text-white rounded-md">
+      <div className=" text-center">
+        <h1 className="text-4xl font-bold mb-3">Get in Touch !</h1>
+        <span>Sign Up for get more update and save your money and time</span>
+        <div className="">
+          <input type="text" placeholder="Your Email" className=" mr-5 my-8 w-4/6 p-2 rounded"/>
+          <button>Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
