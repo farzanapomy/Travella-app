a084e250ec6708d0547fbfe61ae7b5cc8a0eb004
farzanapomy <farzanapomy56@gmail.com>
2024-06-21 03:11:39 +0600

    sub-done

--- app/src/pages/Components/Subscribe/Subscribe.jsx
+++ app/src/pages/Components/Subscribe/Subscribe.jsx
@@ -5,8 +5,14 @@ const Subscribe = () => {
         <h1 className="text-4xl font-bold mb-3">Get in Touch !</h1>
         <span>Sign Up for get more update and save your money and time</span>
         <div className="">
-          <input type="text" placeholder="Your Email" className=" mr-5 my-8 w-4/6 p-2 rounded"/>
-          <button>Subscribe</button>
+          <input
+            type="text"
+            placeholder="Your Email"
+            className=" mr-4 my-8 w-4/6 p-2 rounded outline-none"
+          />
+          <button className="bg-[#91CB82] py-2.5 px-5  rounded-md ">
+            Subscribe
+          </button>
         </div>
       </div>
     </div>
