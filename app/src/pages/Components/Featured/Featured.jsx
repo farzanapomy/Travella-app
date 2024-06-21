7459f5b548d8407c615043a53c582b1d5dabbbaa
farzanapomy <farzanapomy56@gmail.com>
2024-06-22 03:40:21 +0600

    footer-up

--- app/src/pages/Components/Featured/Featured.jsx
+++ app/src/pages/Components/Featured/Featured.jsx
@@ -1,66 +1,70 @@
 import feature1 from '../../../assets/feature1.jpg';
 import feature2 from '../../../assets/feature2.jpeg';
 import feature3 from '../../../assets/feature3.jpg';
+import LayOut from '../LayOut/LayOut';
 const Featured = () => {
   return (
-    <div className="w-full mx-auto my-2">
-
-      <h1 className="font-bold text-4xl  text-start mb-7">Browse By Feature </h1>
-      <div className="flex flex-wrap lg:flex-nowrap justify-center lg:justify-between items-center gap-5 w-full mx-auto    -z-1 ">
-        <div className="cursor-pointer relative  ">
-          <div className=" ">
-            <img
-              src={feature1}
-              alt=""
-              className="rounded-md h-80 cursor-pointer w-96 object-cover "
-            />
+    <LayOut>
+      <div className="w-full mx-auto my-2">
+        <h1 className="font-bold text-4xl  text-start mb-7">
+          Browse By Feature{' '}
+        </h1>
+        <div className="flex flex-wrap lg:flex-nowrap justify-center lg:justify-between items-center gap-5 w-full mx-auto    -z-1 ">
+          <div className="cursor-pointer relative  ">
+            <div className=" ">
+              <img
+                src={feature1}
+                alt=""
+                className="rounded-md h-80 cursor-pointer w-96 object-cover "
+              />
+            </div>
+            <div className="text-white  absolute bottom-5 p-2">
+              <h1 className="font-bold tracking-wider	 text-3xl">Thailand</h1>
+              <h2 className="text-title-md">102 properties</h2>
+            </div>
           </div>
-          <div className="text-white  absolute bottom-5 p-2">
-            <h1 className="font-bold tracking-wider	 text-3xl">Thailand</h1>
-            <h2 className="text-title-md">102 properties</h2>
+          <div className="cursor-pointer relative  ">
+            <div className=" ">
+              <img
+                src={feature2}
+                alt=""
+                className="rounded-md h-80 cursor-pointer w-96 object-cover "
+              />
+            </div>
+            <div className="text-white  absolute bottom-5 p-2">
+              <h1 className="font-bold tracking-wider text-3xl">Japan</h1>
+              <h2 className="text-title-md">102 properties</h2>
+            </div>
           </div>
-        </div>
-        <div className="cursor-pointer relative  ">
-          <div className=" ">
-            <img
-              src={feature2}
-              alt=""
-              className="rounded-md h-80 cursor-pointer w-96 object-cover "
-            />
+          <div className="cursor-pointer relative  ">
+            <div className=" ">
+              <img
+                src={feature3}
+                alt=""
+                className="rounded-md h-80 cursor-pointer w-96 object-cover "
+              />
+            </div>
+            <div className="text-white  absolute bottom-5 p-2">
+              <h1 className="font-bold tracking-wider	 text-3xl">Singapore</h1>
+              <h2 className="text-title-md">102 properties</h2>
+            </div>
           </div>
-          <div className="text-white  absolute bottom-5 p-2">
-            <h1 className="font-bold tracking-wider text-3xl">Japan</h1>
-            <h2 className="text-title-md">102 properties</h2>
-          </div>
-        </div>
-        <div className="cursor-pointer relative  ">
-          <div className=" ">
-            <img
-              src={feature3}
-              alt=""
-              className="rounded-md h-80 cursor-pointer w-96 object-cover "
-            />
-          </div>
-          <div className="text-white  absolute bottom-5 p-2">
-            <h1 className="font-bold tracking-wider	 text-3xl">Singapore</h1>
-            <h2 className="text-title-md">102 properties</h2>
-          </div>
-        </div>
-        <div className="cursor-pointer relative  ">
-          <div className=" ">
-            <img
-              src={feature1}
-              alt=""
-              className="rounded-md h-80 cursor-pointer w-96 object-cover "
-            />
-          </div>
-          <div className="text-white  absolute bottom-5 p-2">
-            <h1 className="font-bold tracking-wider	 text-3xl">Dubai</h1>
-            <h2 className="text-title-md">102 properties</h2>
+          <div className="cursor-pointer relative  ">
+            <div className=" ">
+              <img
+                src={feature1}
+                alt=""
+                className="rounded-md h-80 cursor-pointer w-96 object-cover "
+              />
+            </div>
+            <div className="text-white  absolute bottom-5 p-2">
+              <h1 className="font-bold tracking-wider	 text-3xl">Dubai</h1>
+              <h2 className="text-title-md">102 properties</h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </LayOut>
   );
 };
 
--- app/src/pages/Components/Footer/Footer.jsx
+++ app/src/pages/Components/Footer/Footer.jsx
@@ -1,60 +1,66 @@
+import LayOut from '../LayOut/LayOut';
 import Logo from '../Logo/Logo';
 
 const Footer = () => {
   return (
     <div className="w-full mx-auto bg-[#BBC9B8]  px-5 py-8 h-full">
-      <Logo className="" />
-      <div className="w-full mx-auto flex justify-between items-center ">
-        <div>
-          <ul>
-            <li className=""></li>
-            <li className="cursor-pointer font-semibold">Countries</li>
-            <li className="cursor-pointer">Regions</li>
-            <li className="cursor-pointer">Cities</li>
-            <li className="cursor-pointer">Hotels</li>
-            <li className="cursor-pointer">Airports</li>
-          </ul>
+      <LayOut>
+         
+        <Logo className="cursor-pointer" />
+        <div className="w-full mx-auto flex justify-between items-center ">
+          <div>
+            <ul className='space-y-3'>
+              <li className=""></li>
+              <li className="cursor-pointer font-semibold">Countries</li>
+              <li className="cursor-pointer">Regions</li>
+              <li className="cursor-pointer">Cities</li>
+              <li className="cursor-pointer">Hotels</li>
+              <li className="cursor-pointer">Airports</li>
+            </ul>
+          </div>
+          <div>
+            <ul className='space-y-3'>
+              <li></li>
+              <li className="cursor-pointer font-semibold">Countries</li>
+              <li className="cursor-pointer">Regions</li>
+              <li className="cursor-pointer">Cities</li>
+              <li className="cursor-pointer">Hotels</li>
+              <li className="cursor-pointer">Airports</li>
+            </ul>
+          </div>
+          <div>
+            <ul className='space-y-3'>
+              <li></li>
+              <li className="cursor-pointer font-semibold">Company</li>
+              <li className="cursor-pointer">About Us</li>
+              <li className="cursor-pointer">Popular Service</li>
+              <li className="cursor-pointer">Service</li>
+              <li className="cursor-pointer">Airports</li>
+            </ul>
+          </div>
+          <div>
+            <ul className='space-y-3'>
+              <li></li>
+              <li className="cursor-pointer font-semibold">Support</li>
+              <li className="cursor-pointer">FAQ</li>
+              <li className="cursor-pointer">Help Center</li>
+              <li className="cursor-pointer">Career</li>
+              <li className="cursor-pointer">Privacy</li>
+            </ul>
+          </div>
+          <div>
+            <ul className='space-y-3'>
+              <li></li>
+              <li className="font-semibold">Contact Info</li>
+              <li>+0913-705-3875</li>
+              <li>elizabethJ@jourrapide.com</li>
+              <li>4808 Skinner Hollow Road Days Creek, OR 97429</li>
+              <li>YK</li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <ul>
-            <li></li>
-            <li className="cursor-pointer font-semibold">Countries</li>
-            <li className="cursor-pointer">Regions</li>
-            <li className="cursor-pointer">Cities</li>
-            <li className="cursor-pointer">Hotels</li>
-            <li className="cursor-pointer">Airports</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li></li>
-            <li className="cursor-pointer font-semibold">Company</li>
-            <li className="cursor-pointer">About Us</li>
-            <li className="cursor-pointer">Popular Service</li>
-            <li className="cursor-pointer">Service</li>
-            <li className="cursor-pointer">Airports</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li></li>
-            <li className="cursor-pointer font-semibold">Support</li>
-            <li className="cursor-pointer">FAQ</li>
-            <li className="cursor-pointer">Help Center</li>
-            <li className="cursor-pointer">Career</li>
-            <li className="cursor-pointer">Privacy</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li></li>
-            <li className="font-semibold">Contact Info</li>
-            <li>+0913-705-3875</li>
-            <li>elizabethJ@jourrapide.com</li>
-            <li>4808 Skinner Hollow Road Days Creek, OR 97429</li>
-          </ul>
-        </div>
-      </div>
+      <p>Company All Right Reserved , 2024</p>
+      </LayOut>
     </div>
   );
 };
