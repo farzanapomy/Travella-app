2dcbe2439b9d7fa7249acfbb2159ae82254586b0
farzanapomy <farzanapomy56@gmail.com>
2024-08-07 13:40:56 +0600

    hotels-up

--- app/src/pages/Containers/Hotels/Hotels.jsx
+++ app/src/pages/Containers/Hotels/Hotels.jsx
@@ -4,7 +4,7 @@ import { DateRange } from 'react-date-range';
 import { useLocation } from 'react-router-dom';
 import Footer from '../../Components/Footer/Footer';
 import Navbar from '../../Components/Navbar/Navbar';
-import SingleHotel from '../SingleHotel/SingleHotel';
+import SearchHotels from '../../Components/SearchHotels/SearchHotels';
 const Hotels = () => {
   const location = useLocation();
   console.log(location);
@@ -18,9 +18,9 @@ const Hotels = () => {
       <div className=" h-fit object-fill  bg-no-repeat opacity-90  bg-blend-multiply">
         <div className="max-w-[1224px] mx-auto ">
           <Navbar />
-          <div className="grid grid-cols-3">
-            <div className="p-3 col-span-2">
-              <SingleHotel />
+          <div className="grid grid-cols-3 gap-5">
+            <div className="py-10 col-span-2">
+              <SearchHotels/>
             </div>
             <div className=" py-10 ">
               <div className="flex px-5 flex-col bg-[#EDEDED] p-3 rounded-md">
@@ -35,7 +35,7 @@ const Hotels = () => {
                 <div className="relative py-1.5 w-full  p-1.5 border border-[#BCD7D3]">
                   <span className="  outline-none rounded-md ">
                     <span onClick={() => setOpenDate(!openDate)}>
-                      {`${moment(date[0]?.startDate).format('MM/DD/YYYY')}`} to{' '}
+                    {`${moment(date[0]?.startDate).format('MM/DD/YYYY')}`} to{' '}
                       {`${moment(date[0]?.endDate).format('MM/DD/YYYY')}`}
                     </span>
                   </span>
@@ -43,7 +43,7 @@ const Hotels = () => {
                     {openDate && (
                       <DateRange
                         editableDateInputs={true}
-                        onChange={(item) => setDate([item.selection])}
+                        onChange={(item) => setDate([item?.selection])}
                         moveRangeOnFirstSelection={false}
                         ranges={date}
                         minDate={new Date()}
@@ -78,7 +78,7 @@ const Hotels = () => {
                     <input
                       type="number"
                       className="  outline-none rounded-md  w-full p-1.5 border border-[#BCD7D3]"
-                      placeholder={option.adult}
+                      placeholder={option?.adult}
                       min={1}
                     />
                   </div>
@@ -87,7 +87,7 @@ const Hotels = () => {
                     <input
                       type="number"
                       className="  outline-none rounded-md  w-full p-1.5 border border-[#BCD7D3]"
-                      placeholder={option.children}
+                      placeholder={option?.children}
                       min={0}
                     />
                   </div>
@@ -96,7 +96,7 @@ const Hotels = () => {
                     <input
                       type="number"
                       className="  outline-none rounded-md  w-full p-1.5 border border-[#BCD7D3]"
-                      placeholder={option.room}
+                      placeholder={option?.room}
                       min={1}
                     />
                   </div>
