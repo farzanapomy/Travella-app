e8ebb5276fc5eda8a538cba2f41f4f8673dae2ff
farzanapomy <farzanapomy56@gmail.com>
2024-08-07 13:40:21 +0600

    up

--- app/src/pages/Components/BrowseHotel/BrowseHotel.jsx
+++ app/src/pages/Components/BrowseHotel/BrowseHotel.jsx
@@ -13,7 +13,7 @@ const BrowseHotel = () => {
           Browse By Different Types of Hotels
         </h1>
         <div className="grid grid-cols-2 md:grid-cols-5 items-center gap-5 w-full mx-auto">
-          <div className="cursor-pointer flex-1">
+          <div className="cursor-pointer flex-1 border border-[#D3D3D3] p-3">
             <img
               src={hotel1}
               alt=""
@@ -22,7 +22,7 @@ const BrowseHotel = () => {
             <h1 className="text-xl font-semibold">Hotel</h1>
             <h2 className="text-sm">1,5000 Hotels</h2>
           </div>
-          <div className="cursor-pointer flex-1">
+          <div className="cursor-pointer flex-1 border border-[#D3D3D3] p-3">
             <img
               src={cottage}
               alt=""
@@ -31,7 +31,7 @@ const BrowseHotel = () => {
             <h1 className="text-xl font-semibold">Cottage</h1>
             <h2 className="text-sm">1,5000 Hotels</h2>
           </div>
-          <div className="cursor-pointer flex-1">
+          <div className="cursor-pointer flex-1 border border-[#D3D3D3] p-3">
             <img
               src={hotel2}
               alt=""
@@ -40,7 +40,7 @@ const BrowseHotel = () => {
             <h1 className="text-xl font-semibold">Apartment</h1>
             <h2 className="text-sm">1,5000 Hotels</h2>
           </div>
-          <div className="cursor-pointer flex-1">
+          <div className="cursor-pointer flex-1 border border-[#D3D3D3] p-3">
             <img
               src={hotel3}
               alt=""
@@ -49,7 +49,7 @@ const BrowseHotel = () => {
             <h1 className="text-xl font-semibold">Resort</h1>
             <h2 className="text-sm">1,5000 Hotels</h2>
           </div>
-          <div className="cursor-pointer flex-1">
+          <div className="cursor-pointer flex-1 border border-[#D3D3D3] p-3">
             <img
               src={hotel4}
               alt=""
