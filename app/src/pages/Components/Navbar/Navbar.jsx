f18535b13b66ed0eb66e702132117543455d2931
farzanapomy <farzanapomy56@gmail.com>
2024-08-10 21:21:45 +0600

    ms

--- app/src/pages/Components/Navbar/Navbar.jsx
+++ app/src/pages/Components/Navbar/Navbar.jsx
@@ -3,20 +3,18 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   console.log(window.location.href);
   return (
-    <div
-      className={`w-full ${
-        window.location.pathname === '/' ? 'bg-none' : 'bg-[#BCD7D3]'
-      } border-opacity-40 mx-auto`}
-    >
+    <div className={`w-full border-opacity-40 mx-auto`}>
       <div className="">
         <div className="flex justify-between items-center">
           <div className="mx-4 mt-10 mb-6">
             <button className="mr-4">About</button>
             <button>Contact</button>
           </div>
-          <Link to="/">
-            <Logo />
-          </Link>
+          <div className={``}>
+            <Link to="/">
+              <Logo />
+            </Link>
+          </div>
           <div className="mx-4 mt-10 mb-6">
             <button className="mr-4">Register</button>
             <button>Login</button>
