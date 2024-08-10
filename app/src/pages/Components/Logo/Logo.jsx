16d2cf5a69a8a8eb5b0ffa890b5a62ebfeed4561
farzanapomy <farzanapomy56@gmail.com>
2024-08-10 21:21:32 +0600

    logo-change

--- app/src/pages/Components/Logo/Logo.jsx
+++ app/src/pages/Components/Logo/Logo.jsx
@@ -1,8 +1,12 @@
 const Logo = () => {
   return (
     <div>
-      <h1 className=" mt-10 mb-6 text-2xl font-Inter font-bold  invert drop-shadow-xl">
-        TrɅ<span className="text-black">vel</span>lɅ Go
+      <h1
+        className={` mt-10 mb-6 text-2xl font-Inter font-bold   drop-shadow-xl  ${
+          window.location.pathname === '/' ? 'invert' : 'text-[#3A5357]'
+        }`}
+      >
+        TrɅ<span className={`text-black`}>vel</span>lɅ Go
       </h1>
     </div>
   );
