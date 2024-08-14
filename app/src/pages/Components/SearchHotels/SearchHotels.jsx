80c93a55044521d923acbfd6448f85a235c63aa6
farzanapomy <farzanapomy56@gmail.com>
2024-08-14 20:19:57 +0600

    search-up

--- app/src/pages/Components/SearchHotels/SearchHotels.jsx
+++ app/src/pages/Components/SearchHotels/SearchHotels.jsx
@@ -1,7 +1,7 @@
 import img from '../../../assets/hotel2.jpg';
 const SearchHotels = () => {
   return (
-    <div className="border border-[#D3D3D3] p-2 shadow-sm cursor-pointer ">
+    <div className="border border-[#D3D3D3] p-2 rounded-lg shadow-xl	">
       <div className="flex flex-col justify-start items-center gap-5 ">
         <div className="">
           <img
@@ -12,31 +12,31 @@ const SearchHotels = () => {
         </div>
         <div className=" flex justify-start items-start flex-grow flex-col pl-1">
           <div className=" flex justify-start items-center">
-            <p className="text-title-md font-semibold mb-2 text-[#3A5357]">
-              Tower Street Apartment
+            <p className="text-title-xsm font-semibold mb-2 text-[#3A5357]">
+              Dubai Tower Apartment
             </p>
-            <div className="flex flex-col items-end justify-end flex-grow">
-              <span className="text-sm text-[#3A5357]">Excellent</span>
-              <button className="text-white bg-[#3A5357] px-1 rounded">
-                4.2
-              </button>
-            </div>
+            <span className="text-2xl text-[#3A5357] font-bold">$123.00</span>
+          </div>
+          <div className="flex flex-col items-end justify-end flex-grow">
+            <span>Includes taxes and fees</span>
           </div>
           <span className="text-sm">500m from center</span>
-          <button className="text-white bg-[#3a5357c4] p-1 rounded text-sm mt-2">
-            Free airport text
-          </button>
-          <span className="font-semibold text-sm my-2">
+          <span className="  px-1 rounded text-sm mt-1">Free airport taxi</span>
+          <span className="font-semibold text-sm my-1">
             Studio Apartment with Air Condition
           </span>
-          <span className="">Entire studio + 1 Bathroom </span>
-          <span className="text-[#3A5357] font-semibold">Free Cancelation</span>
-          <span className="text-[#3A5357] text-sm">
-            You can cancel later also so please booked now to get great price
-            now!!
+          <span className="text-sm">Entire studio + 1 Bathroom </span>
+          <span className="text-[#3A5357] font-semibold">
+            Free Cancelation Service
           </span>
+          {/* <span className="text-[#3A5357] text-sm">
+            You can cancel later, but for now, book to secure a great price! 
+          </span>
+            */}
         </div>
-        <div className="flex justify-between items-center gap-5"></div>
+        <button className=" p-1 rounded-md flex-1 bg-[#3A5357] w-full text-white mx-auto text-center">
+          Check Availability
+        </button>
       </div>
     </div>
   );
