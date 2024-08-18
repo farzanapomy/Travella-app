79ce9c3db12e468833fb8a18bcc85739ec48cc10
farzanapomy <farzanapomy56@gmail.com>
2024-08-18 19:30:02 +0600

    done-cat

--- app/src/pages/Components/Category/Category.jsx
+++ app/src/pages/Components/Category/Category.jsx
@@ -2,35 +2,66 @@ import tourGuide from '../../../assets/tour-guide 1.png';
 import travelling from '../../../assets/travelling 1.png';
 import hands from '../../../assets/hands 1.png';
 import medical from '../../../assets/medical-team 1.png';
+import LayOut from '../LayOut/LayOut';
 const Category = () => {
   return (
-    <div>
-      <h2>CATEGORY</h2>
-      <h1>We Offer Best Services</h1>
+    <LayOut>
+      <div className="w-full mx-auto my-2 ">
+        <div className="text-center mb-9 font-[volkhov]">
+          <h2 className="text-2xl">CATEGORY</h2>
+          <h1 className="text-4xl font-semibold my-2">
+            We Offer Best Services
+          </h1>
+        </div>
 
-      <div>
-        <div>
-          <img src={tourGuide} alt="" />
-          <h3>Guided Tours</h3>
-          <p>sunt qui repellat saepe quo velit aperiam id aliquam placeat.</p>
-        </div>
-        <div>
-          <img src={travelling} alt="" />
-          <h3>Best Flights Options</h3>
-          <p>sunt qui repellat saepe quo velit aperiam id aliquam placeat.</p>
-        </div>
-        <div>
-          <img src={hands} alt="" />
-          <h3>Religious Tours</h3>
-          <p>sunt qui repellat saepe quo velit aperiam id aliquam placeat.</p>
-        </div>
-        <div>
-          <img src={medical} alt="" />
-          <h3>Medical insurance</h3>
-          <p>sunt qui repellat saepe quo velit aperiam id aliquam placeat.</p>
+        <div className="grid grid-cols-4 gap-4 items-center">
+          <div className="w-50 hover:bg-[#FFFFFF] hover:shadow-lg p-5 rounded mx-auto">
+            <div className="mx-auto w-20 rounded-full  ">
+              <img src={tourGuide} alt="" />
+            </div>
+            <div className="text-center mt-2">
+              <h3 className="font-semibold my-2">Guided Tours</h3>
+              <p className="">
+                sunt qui repellat saepe quo velit aperiam id aliquam placeat.
+              </p>
+            </div>
+          </div>
+          <div className="w-50 hover:bg-[#FFFFFF] hover:shadow-lg p-5 rounded mx-auto">
+            <div className="mx-auto w-20 rounded-full  ">
+              <img src={hands} alt="" />
+            </div>
+            <div className="text-center mt-2">
+              <h3 className="font-semibold my-2">Religious Tours</h3>
+              <p className="">
+                sunt qui repellat saepe quo velit aperiam id aliquam placeat.
+              </p>
+            </div>
+          </div>
+          <div className="w-50 hover:bg-[#FFFFFF] hover:shadow-lg p-5 rounded mx-auto">
+            <div className="mx-auto w-20 rounded-full  ">
+              <img src={travelling} alt="" />
+            </div>
+            <div className="text-center mt-2">
+              <h3 className="font-semibold my-2">Best Flights Options</h3>
+              <p className="">
+                sunt qui repellat saepe quo velit aperiam id aliquam placeat.
+              </p>
+            </div>
+          </div>
+          <div className="w-50 hover:bg-[#FFFFFF] hover:shadow-lg p-5 rounded mx-auto">
+            <div className="mx-auto w-20 rounded-full  ">
+              <img src={medical} alt="" />
+            </div>
+            <div className="text-center mt-2">
+              <h3 className="font-semibold my-2">Medical insurance</h3>
+              <p className="">
+                sunt qui repellat saepe quo velit aperiam id aliquam placeat.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </LayOut>
   );
 };
 
