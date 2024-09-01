be5923552c2ddc0b6562c76ebc4a47fbdd024864
farzanapomy <farzanapomy56@gmail.com>
2024-09-01 23:01:44 +0600

    up-nav

--- app/src/pages/Components/Navbar/Navbar.jsx
+++ app/src/pages/Components/Navbar/Navbar.jsx
@@ -1,16 +1,23 @@
 import Logo from '../Logo/Logo';
 import { Link } from 'react-router-dom';
+import { MdOutlineFlightTakeoff } from 'react-icons/md';
+import { IoBed } from 'react-icons/io5';
+
 const Navbar = () => {
   console.log(window.location.href);
   return (
-    <div
-      className={`w-full border-opacity-40 mx-auto  `}
-    >
+    <div className={`w-full border-opacity-40 mx-auto  `}>
       <div className="">
         <div className="flex justify-between items-center">
           <div className="mx-4 mt-10 mb-6">
-            <button className="mr-4">About</button>
-            <button>Contact</button>
+            <button className="mr-4">
+              <MdOutlineFlightTakeoff />
+              Find Flight
+            </button>
+            <button>
+              <IoBed />
+              Find Stays
+            </button>
           </div>
           <div className={``}>
             <Link to="/">
--- app/src/pages/Components/SearchHotels/SearchHotels.jsx
+++ app/src/pages/Components/SearchHotels/SearchHotels.jsx
@@ -1,27 +1,30 @@
-import img from '../../../assets/hotel2.jpg';
+import { useNavigate } from 'react-router-dom';
+import apart from '../../../assets/apart.jpg';
 const SearchHotels = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="border border-[#D3D3D3] p-2 rounded-lg shadow-xl	">
       <div className="flex flex-col justify-start items-center gap-5 ">
         <div className="">
           <img
-            src={img}
+            src={apart}
             alt=""
             className="rounded-md h-45 object-cover w-full"
           />
         </div>
         <div className=" flex justify-start items-start flex-grow flex-col pl-1">
           <div className=" flex justify-start items-center">
-            <p className="text-title-xsm font-semibold mb-2 text-[#3A5357]">
+            <p className="text-title-sm font-semibold mb-2 text-[#3A5357]">
               Dubai Tower Apartment
             </p>
-            <span className="text-2xl text-[#3A5357] font-bold">$123.00</span>
           </div>
+          <span className="text-2xl   font-bold">$123.00</span>
           <div className="flex flex-col items-end justify-end flex-grow">
             <span>Includes taxes and fees</span>
           </div>
           <span className="text-sm">500m from center</span>
-          <span className="  px-1 rounded text-sm mt-1">Free airport taxi</span>
+          <span className="rounded text-sm mt-1">Free airport taxi</span>
           <span className="font-semibold text-sm my-1">
             Studio Apartment with Air Condition
           </span>
@@ -34,7 +37,10 @@ const SearchHotels = () => {
           </span>
             */}
         </div>
-        <button className=" p-1 rounded-md flex-1 bg-[#3A5357] w-full text-white mx-auto text-center">
+        <button
+          className=" p-1 rounded-md flex-1 bg-[#3A5357] w-full text-white mx-auto text-center"
+          onClick={() => navigate(`/hotels/${1}`)}
+        >
           Check Availability
         </button>
       </div>
