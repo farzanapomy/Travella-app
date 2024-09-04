61d0aea41ca72445ce1922ca0aac8ac92fc44563
farzanapomy <farzanapomy56@gmail.com>
2024-09-04 20:53:41 +0600

    update-package

--- api/src/index.js
+++ api/src/index.js
@@ -7,9 +7,11 @@ import hotelsRoute from './routes/hotels.js';
 import roomsRoute from './routes/room.js';
 import cookieParser from 'cookie-parser';
 const app = express();
+import cors from 'cors';
 app.use(express.json());
 app.use(cookieParser());
 dotenv.config();
+app.use(cors());
 handleConnection();
 
 app.use('/api/v1/auth', authRoute);
@@ -17,7 +19,7 @@ app.use('/api/v1/users', usersRoute);
 app.use('/api/v1/hotels', hotelsRoute);
 app.use('/api/v1/rooms', roomsRoute);
 // error handlers
-app.use((err, req, res, next) => {        
+app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
   const errorMessage = err.message || 'Something went wrong';
   return res
