8eb6b9f849aa0f968c7b0edf89e37d2bf623f3ea
farzanapomy <farzanapomy56@gmail.com>
2024-09-06 23:02:34 +0600

    update-hotel

--- app/src/pages/Containers/SingleHotel/SingleHotel.jsx
+++ app/src/pages/Containers/SingleHotel/SingleHotel.jsx
@@ -1,59 +1,84 @@
-import hotel1 from '../../../assets/hotel1.jpg';
-import hotel2 from '../../../assets/hotel2.jpg';
-import hotel3 from '../../../assets/hotel3.jpg';
-import hotel4 from '../../../assets/hotel4.jpg';
-import cottage from '../../../assets/cottage.jpg';
-
+import Footer from '../../Components/Footer/Footer';
+import Navbar from '../../Components/Navbar/Navbar';
+import Subscribe from '../../Components/Subscribe/Subscribe';
+import img from '../../../assets/apart.jpg';
+import img2 from '../../../assets/apart1.jpg';
+import img3 from '../../../assets/apart2.jpg';
 const SingleHotel = () => {
   return (
-    <div className='p-10 pl-0'>
-      <div className="grid grid-cols-2 md:grid-cols-3 items-center gap-5 w-full mx-auto">
-        <div className="cursor-pointer flex-1">
-          <img
-            src={hotel1}
-            alt=""
-            className="rounded-md  h-40 object-cover w-full "
-          />
-          <h1 className="text-xl font-semibold">Hotel</h1>
-          <h2 className="text-sm">1,5000 Hotels</h2>
+    <div className="max-w-[1224px] mx-auto">
+      <Navbar />
+      <div className="p-3">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col justify-start items-start">
+            <p className="text-title-sm font-semibold mb-2 text-[#3A5357]">
+              Dubai Tower Apartment
+            </p>
+            <p>4808 Skinner Hollow Road Days Creek, OR 97429, YK</p>
+            <p>Excellent Location</p>
+            <div>
+              <span>Rating :</span>
+              <button className=" text-white  bg-[#3A5357] p-1 rounded">
+                4.2
+              </button>
+            </div>
+            <span>
+              Book a stay over $123 at this property and get a free airport taxi
+            </span>
+          </div>
+          <button className=" p-1 rounded-md flex-1 bg-[#3A5357] w-5 text-white mx-auto text-center">
+            Reverse Or Book Now
+          </button>
         </div>
-        <div className="cursor-pointer flex-1">
-          <img
-            src={cottage}
-            alt=""
-            className="rounded-md  h-40 object-cover w-full "
-          />
-          <h1 className="text-xl font-semibold">Cottage</h1>
-          <h2 className="text-sm">1,5000 Hotels</h2>
-        </div>
-        <div className="cursor-pointer flex-1">
-          <img
-            src={hotel2}
-            alt=""
-            className="rounded-md  h-40 object-cover w-full "
-          />
-          <h1 className="text-xl font-semibold">Apartment</h1>
-          <h2 className="text-sm">1,5000 Hotels</h2>
-        </div>
-        <div className="cursor-pointer flex-1">
-          <img
-            src={hotel3}
-            alt=""
-            className="rounded-md  h-40 object-cover w-full "
-          />
-          <h1 className="text-xl font-semibold">Resort</h1>
-          <h2 className="text-sm">1,5000 Hotels</h2>
-        </div>
-        <div className="cursor-pointer flex-1">
-          <img
-            src={hotel4}
-            alt=""
-            className="rounded-md  h-40 object-cover w-full "
-          />
-          <h1 className="text-xl font-semibold">Cabins</h1>
-          <h2 className="text-sm">1,5000 Hotels</h2>
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 my-4">
+          <div>
+            <img
+              src={img}
+              alt=""
+              className="rounded-md h-70  object-cover w-full "
+            />
+          </div>
+          <div>
+            {' '}
+            <img
+              src={img2}
+              alt=""
+              className="rounded-md h-70  object-cover w-full "
+            />
+          </div>
+          <div>
+            <img
+              src={img3}
+              alt=""
+              className="rounded-md h-70   object-cover w-full "
+            />
+          </div>
+          <div>
+            <img
+              src={img3}
+              alt=""
+              className="rounded-md h-70  object-cover w-full "
+            />
+          </div>
+          <div>
+            {' '}
+            <img
+              src={img}
+              alt=""
+              className="rounded-md h-70  object-cover w-full "
+            />
+          </div>
+          <div>
+            <img
+              src={img2}
+              alt=""
+              className="rounded-md h-70   object-cover w-full "
+            />
+          </div>
         </div>
       </div>
+      <Subscribe />
+      <Footer />
     </div>
   );
 };
