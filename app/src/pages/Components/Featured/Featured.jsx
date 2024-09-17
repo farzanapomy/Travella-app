224b35e12470013a806b62e6701368805ce40801
farzanapomy <farzanapomy56@gmail.com>
2024-09-17 21:24:54 +0600

    update-api

--- app/src/pages/Components/Featured/Featured.jsx
+++ app/src/pages/Components/Featured/Featured.jsx
@@ -13,60 +13,68 @@ const Featured = () => {
         <h1 className="font-bold text-4xl  text-start mb-7">
           Browse By Feature{' '}
         </h1>
-        <div className="flex flex-wrap lg:flex-nowrap justify-center lg:justify-between items-center gap-5 w-full mx-auto    -z-1 ">
-          <div className="cursor-pointer relative  ">
-            <div className=" ">
-              <img
-                src={feature1}
-                alt=""
-                className="rounded-md h-80 cursor-pointer w-96 object-cover "
-              />
+        {loading ? (
+          <p>Loading! Please wait.</p>
+        ) : error ? (
+          <p>Something went wrong</p>
+        ) : data && data.length > 1 ? (
+          <div className="flex flex-wrap lg:flex-nowrap justify-center lg:justify-between items-center gap-5 w-full mx-auto -z-1">
+            <div className="cursor-pointer relative">
+              <div>
+                <img
+                  src={feature1}
+                  alt="Thailand"
+                  className="rounded-md h-80 cursor-pointer w-96 object-cover"
+                />
+              </div>
+              <div className="text-white absolute bottom-5 p-2">
+                <h1 className="font-bold tracking-wider text-3xl">USA</h1>
+                <h2 className="text-title-md">{data[0]} properties</h2>
+              </div>
             </div>
-            <div className="text-white  absolute bottom-5 p-2">
-              <h1 className="font-bold tracking-wider	 text-3xl">Thailand</h1>
-              <h2 className="text-title-md">102 properties</h2>
+            <div className="cursor-pointer relative">
+              <div>
+                <img
+                  src={feature2}
+                  alt="Japan"
+                  className="rounded-md h-80 cursor-pointer w-96 object-cover"
+                />
+              </div>
+              <div className="text-white absolute bottom-5 p-2">
+                <h1 className="font-bold tracking-wider text-3xl">Japan</h1>
+                <h2 className="text-title-md">{data[1]} properties</h2>
+              </div>
+            </div>
+            <div className="cursor-pointer relative">
+              <div>
+                <img
+                  src={feature3}
+                  alt="Singapore"
+                  className="rounded-md h-80 cursor-pointer w-96 object-cover"
+                />
+              </div>
+              <div className="text-white absolute bottom-5 p-2">
+                <h1 className="font-bold tracking-wider text-3xl">New York</h1>
+                <h2 className="text-title-md">{data[2]} properties</h2>
+              </div>
+            </div>
+            <div className="cursor-pointer relative">
+              <div>
+                <img
+                  src={feature1}
+                  alt="Dubai"
+                  className="rounded-md h-80 cursor-pointer w-96 object-cover"
+                />
+              </div>
+              <div className="text-white absolute bottom-5 p-2">
+                <h1 className="font-bold tracking-wider text-3xl">Dubai</h1>
+                <h2 className="text-title-md">{data[3]} properties</h2>
+              </div>
             </div>
           </div>
-          <div className="cursor-pointer relative  ">
-            <div className=" ">
-              <img
-                src={feature2}
-                alt=""
-                className="rounded-md h-80 cursor-pointer w-96 object-cover "
-              />
-            </div>
-            <div className="text-white  absolute bottom-5 p-2">
-              <h1 className="font-bold tracking-wider text-3xl">Japan</h1>
-              <h2 className="text-title-md">102 properties</h2>
-            </div>
-          </div>
-          <div className="cursor-pointer relative  ">
-            <div className=" ">
-              <img
-                src={feature3}
-                alt=""
-                className="rounded-md h-80 cursor-pointer w-96 object-cover "
-              />
-            </div>
-            <div className="text-white  absolute bottom-5 p-2">
-              <h1 className="font-bold tracking-wider	 text-3xl">Singapore</h1>
-              <h2 className="text-title-md">102 properties</h2>
-            </div>
-          </div>
-          <div className="cursor-pointer relative  ">
-            <div className=" ">
-              <img
-                src={feature1}
-                alt=""
-                className="rounded-md h-80 cursor-pointer w-96 object-cover "
-              />
-            </div>
-            <div className="text-white  absolute bottom-5 p-2">
-              <h1 className="font-bold tracking-wider	 text-3xl">Dubai</h1>
-              <h2 className="text-title-md">102 properties</h2>
-            </div>
-          </div>
-        </div>
+        ) : (
+          <p>No Data Available</p>
+        )}
       </div>
     </LayOut>
   );
