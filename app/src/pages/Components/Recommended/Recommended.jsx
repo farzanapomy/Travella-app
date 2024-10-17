6b7c478a835afb4b6f3a48c5e66f2cf403aac851
farzanapomy <farzanapomy56@gmail.com>
2024-10-17 12:13:55 +0600

    change-ui

--- app/src/pages/Components/Recommended/Recommended.jsx
+++ app/src/pages/Components/Recommended/Recommended.jsx
@@ -4,126 +4,62 @@ import rating3 from '../../../assets/rating3.jpg';
 import rating4 from '../../../assets/rating4.jpg';
 import { CiStar } from 'react-icons/ci';
 import LayOut from '../LayOut/LayOut';
+import useFetch from '../../../hooks/useFatch';
+import { GETSEARCHDATA } from '../../../urls/urls';
 
 const Recommended = () => {
+  const { data, loading, error } = useFetch(GETSEARCHDATA);
+  console.log(data);
+  const images = [rating1, rating2, rating3, rating4];
   return (
     <LayOut>
       <div className="w-full mx-auto my-2 mt-10">
         <div className="text-center mb-9 font-[volkhov]">
           <h1 className="text-3xl mb-1 uppercase">Recommended</h1>
           <h1 className="font-bold text-3xl mb-7">
-            Browse All the Recommended Hotels By Client{' '}
+            Browse All the Recommended Hotels By Client
           </h1>
         </div>
         <div className="flex flex-wrap lg:flex-nowrap justify-center lg:justify-between items-center gap-5 w-full mx-auto">
-          <div className="cursor-pointer border border-[#D3D3D3] p-3 shadow-md">
-            <img
-              src={rating1}
-              alt=""
-              className="rounded-md h-60 object-cover w-full "
-            />
-            <p className=" font-semibold text-2xl">Hotel of Dubai</p>
-            <div className="flex mt-2 justify-between gap-10">
-              <p className=" ">Dubai</p>
-              <p className=" font-semibold text-xl ">$1500</p>
-            </div>
-            <div className="flex mt-2 justify-between items-center gap-3">
-              <div className="flex gap-2 justify-start items-center">
-                <button className=" text-white  bg-[#3A5357] p-1 rounded">
-                  4.2
-                </button>
-                <div className="flex justify-start items-center">
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
+          {loading
+            ? 'loading'
+            : data &&
+              images.map((img, i) => (
+                <div
+                  className="cursor-pointer border border-[#D3D3D3] p-3 shadow-md"
+                  key={i}
+                >
+                  <img
+                    src={img}
+                    alt=""
+                    className="rounded-md h-60 object-cover w-full "
+                  />
+                  <p className=" font-semibold text-xl">{data[i]?.name}</p>
+                  <div className="flex mt-2 justify-between gap-10">
+                    <p className=" ">{data[i]?.city}</p>
+                    <p className=" font-semibold text-xl ">
+                      ${data[i]?.cheapestPrice}
+                    </p>
+                  </div>
+                  <div className="flex mt-2 justify-between items-center gap-3">
+                    <div className="flex gap-2 justify-start items-center">
+                      <button className=" text-white  bg-[#3A5357] p-1 rounded">
+                        4.2
+                      </button>
+                      <div className="flex justify-start items-center">
+                        <CiStar />
+                        <CiStar />
+                        <CiStar />
+                        <CiStar />
+                        <CiStar />
+                      </div>
+                    </div>
+                    <span className="text-title-md text-[#3A5357]">
+                      Excellent
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <span className="text-title-md text-[#3A5357]">Excellent</span>
-            </div>
-          </div>
-          <div className="cursor-pointer border border-[#D3D3D3] p-3 shadow-md">
-            <img
-              src={rating2}
-              alt=""
-              className="rounded-md h-60 object-cover w-full "
-            />
-            <p className=" font-semibold text-2xl">Hotel of Dubai</p>
-            <div className="flex mt-2 justify-between gap-10">
-              <p className=" ">Dubai</p>
-              <p className=" font-semibold text-xl ">$1500</p>
-            </div>
-            <div className="flex mt-2 justify-between items-center gap-3">
-              <div className="flex gap-2 justify-start items-center">
-                <button className=" text-white  bg-[#3A5357] p-1 rounded">
-                  4.2
-                </button>
-                <div className="flex justify-start items-center">
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                </div>
-              </div>
-              <span className="text-title-md text-[#3A5357]">Excellent</span>
-            </div>
-          </div>
-          <div className="cursor-pointer border border-[#D3D3D3] p-3 shadow-md">
-            <img
-              src={rating3}
-              alt=""
-              className="rounded-md h-60 object-cover w-full "
-            />
-            <p className=" font-semibold text-2xl">Hotel of Dubai</p>
-            <div className="flex mt-2 justify-between gap-10">
-              <p className=" ">Dubai</p>
-              <p className=" font-semibold text-xl ">$1500</p>
-            </div>
-            <div className="flex mt-2 justify-between items-center gap-3">
-              <div className="flex gap-2 justify-start items-center">
-                <button className=" text-white  bg-[#3A5357] p-1 rounded">
-                  4.2
-                </button>
-                <div className="flex justify-start items-center">
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                </div>
-              </div>
-              <span className="text-title-md text-[#3A5357]">Excellent</span>
-            </div>
-          </div>
-          <div className="cursor-pointer border border-[#D3D3D3] p-3 shadow-md">
-            <img
-              src={rating4}
-              alt=""
-              className="rounded-md h-60 object-cover w-full "
-            />
-            <p className=" font-semibold text-2xl">Hotel of Dubai</p>
-            <div className="flex mt-2 justify-between gap-10">
-              <p className=" ">Dubai</p>
-              <p className=" font-semibold text-xl ">$1500</p>
-            </div>
-            <div className="flex mt-2 justify-between items-center gap-3">
-              <div className="flex gap-2 justify-start items-center">
-                <button className=" text-white  bg-[#3A5357] p-1 rounded">
-                  4.2
-                </button>
-                <div className="flex justify-start items-center">
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                  <CiStar />
-                </div>
-              </div>
-              <span className="text-title-md text-[#3A5357]">Excellent</span>
-            </div>
-          </div>
+              ))}
         </div>
       </div>
     </LayOut>
