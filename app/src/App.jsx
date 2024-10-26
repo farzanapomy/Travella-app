34f1c837a9a730f48e8b1e59da46e84d47b6a451
farzanapomy <farzanapomy56@gmail.com>
2024-10-26 18:48:04 +0600

    change-ip

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,13 +4,14 @@ import Hotels from './pages/Containers/Hotels/Hotels';
 import SingleHotel from './pages/Containers/SingleHotel/SingleHotel';
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hotels" element={<Hotels />} />
-        <Route path="/hotels/:id" element={<SingleHotel />} />
-      </Routes>
-    </BrowserRouter>
+   
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<Hotels />} />
+          <Route path="/hotels/:id" element={<SingleHotel />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
