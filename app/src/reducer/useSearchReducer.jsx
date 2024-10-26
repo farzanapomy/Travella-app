537b423a29f0f3680b39f07276b0804808d30a8b
farzanapomy <farzanapomy56@gmail.com>
2024-10-26 18:48:24 +0600

    manage-state

---
+++ app/src/reducer/useSearchReducer.jsx
@@ -0,0 +1,44 @@
+import { createContext, useReducer } from 'react';
+
+const INITIAL_STATE = {
+  destination: undefined,
+  date: [],
+  option: {
+    adult: undefined,
+    child: undefined,
+    room: undefined,
+  },
+};
+
+// Create the context with a default value that includes state and dispatch
+export const SearchContext = createContext(INITIAL_STATE);
+
+// Reducer function
+const SearchAction = (state, action) => {
+  switch (action.type) {
+    case 'NEW_SEARCH_STATE':
+      return action.payload;
+    case 'UPDATE_SEARCH_STATE':
+      return INITIAL_STATE;
+    default:
+      return state;
+  }
+};
+
+// Context provider component
+export const SearchContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(SearchAction, INITIAL_STATE);
+
+  return (
+    <SearchContext.Provider
+      value={{
+        destination: state.destination,
+        date: state.date,
+        option: state.option,
+        dispatch,
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+};
