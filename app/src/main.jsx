1de83af400541bf08b50b62d187dd63aa1ec94bf
farzanapomy <farzanapomy56@gmail.com>
2024-10-31 18:49:25 +0600

    wrap-app

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import './App.css'
+import './App.css';
+import { SearchContextProvider } from './reducer/useSearchReducer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SearchContextProvider>
+      <App />
+    </SearchContextProvider>
   </React.StrictMode>
 );
