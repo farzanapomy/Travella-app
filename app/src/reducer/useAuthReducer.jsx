9c6ddb81ac7de30416395840f50db1f8d01737ce
farzanapomy <farzanapomy56@gmail.com>
2024-11-06 01:25:02 +0600

    create-auth

---
+++ app/src/reducer/useAuthReducer.jsx
@@ -0,0 +1,58 @@
+import { createContext, useEffect, useReducer } from 'react';
+
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem('user')) || null,
+  loading: false,
+  error: null,
+};
+
+// Create the context with a default value that includes state and dispatch
+export const AuthContext = createContext(INITIAL_STATE);
+
+// Reducer function
+const AuthAction = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN_START':
+      return {
+        user: null,
+        loading: true,
+        error: null,
+      };
+    case 'LOGIN_SUCCESS':
+      return {
+        user: action.payload,
+        loading: false,
+        error: null,
+      };
+    case 'LOGIN_ERROR':
+      return {
+        user: null,
+        loading: false,
+        error: action.payload,
+      };
+    case 'LOGOUT':
+      return INITIAL_STATE;
+    default:
+      return state;
+  }
+};
+
+// Context provider component
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthAction, INITIAL_STATE);
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(state.user));
+  }, [state.user]);
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        loading: state.loading,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
