7a758836d7a4637eb81dbb97f87c4d19b6ffda5c
farzanapomy <farzanapomy56@gmail.com>
2024-11-06 01:24:47 +0600

    add-reducer

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx';
 import './index.css';
 import './App.css';
 import { SearchContextProvider } from './reducer/useSearchReducer.jsx';
+import { AuthContextProvider } from './reducer/useAuthReducer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SearchContextProvider>
-      <App />
-    </SearchContextProvider>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
