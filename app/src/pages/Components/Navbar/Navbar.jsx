c652df49219429f576b211e58637ab6a52ec59fb
farzanapomy <farzanapomy56@gmail.com>
2024-11-14 12:03:58 +0600

    change-nav

--- app/src/pages/Components/Navbar/Navbar.jsx
+++ app/src/pages/Components/Navbar/Navbar.jsx
@@ -2,9 +2,12 @@ import Logo from '../Logo/Logo';
 import { Link } from 'react-router-dom';
 import { MdOutlineFlightTakeoff } from 'react-icons/md';
 import { IoBed } from 'react-icons/io5';
+import { useContext } from 'react';
+import { AuthContext } from '../../../reducer/useAuthReducer';
 
 const Navbar = () => {
-  console.log(window.location.href);
+  const { user } = useContext(AuthContext);
+  console.log(user);
   return (
     <div className={`w-full border-opacity-40 mx-auto  `}>
       <div className="">
@@ -24,14 +27,20 @@ const Navbar = () => {
               <Logo />
             </Link>
           </div>
-          <div className="mx-4 mt-10 mb-6">
-            <Link to={'/register'}>
-              <button className="mr-4">Register</button>{' '}
-            </Link>
-            <Link to={'/login'}>
-              <button>Login</button>
-            </Link>
-          </div>
+          {user ? (
+            <>
+              <button>Logout</button>
+            </>
+          ) : (
+            <div className="mx-4 mt-10 mb-6">
+              <Link to={'/register'}>
+                <button className="mr-4">Register</button>{' '}
+              </Link>
+              <Link to={'/login'}>
+                <button >Login</button>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
