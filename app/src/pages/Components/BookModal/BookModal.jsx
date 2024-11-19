c0e75b7db09e0b5bbd6f3e98654b2429e04da480
farzanapomy <farzanapomy56@gmail.com>
2024-11-20 02:48:49 +0600

    up-modal

--- app/src/pages/Components/BookModal/BookModal.jsx
+++ app/src/pages/Components/BookModal/BookModal.jsx
@@ -1,6 +1,16 @@
+import { MdOutlineCancel } from 'react-icons/md';
 const BookModal = ({ setOpenModal, id }) => {
   console.log(id);
-  return <div className="absolute inset-0">Hello </div>;
+  return (
+    <div className="absolute inset-0">
+      <div>
+        <MdOutlineCancel onClick={() => setOpenModal(false)} />
+      </div> 
+      <div>
+        <span>Select Your Rooms: </span>
+      </div>
+    </div>
+  );
 };
 
 export default BookModal;
